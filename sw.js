974001775baaf187edbb38439f836be4b0b00205
sywee-42880999 <inawajuna@gmail.com>
2026-09-07 16:40:39 +0900

    Promote approved BLACK learning UI to root

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE = 'car-master-v05';
+const CACHE = 'car-master-black-v06';
 const ASSETS = [
   './',
   './index.html',
+  './black-preview.html',
+  './black-preview.css?v=1',
+  './black-preview.js?v=1',
   './app.js?v=5',
   './style.css?v=5',
   './style-v04.css?v=5',
   './manifest.webmanifest',
   './data/parts.json',
+  './data/master.json',
+  './data/source_registry.json',
   './data/vehicles.json',
   './images/venue/front.jpg',
   './images/venue/rear.jpg',
